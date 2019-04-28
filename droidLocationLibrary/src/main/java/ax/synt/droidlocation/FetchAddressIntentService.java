3eb9726db218d34476f74c09f1abb07806a32428
JobGetabu <getabujob@gmail.com>
2019-04-28 18:26:30 +0300

    created a FetchAddressIntentService

---
+++ droidLocationLibrary/src/main/java/ax/synt/droidlocation/FetchAddressIntentService.java
@@ -0,0 +1,98 @@
+package ax.synt.droidlocation;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.ResultReceiver;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class FetchAddressIntentService extends IntentService {
+
+    private String TAG = "AddressService";
+
+    protected ResultReceiver receiver;
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public FetchAddressIntentService(String name) {
+        super(name);
+    }
+
+    // ...
+    private void deliverResultToReceiver(int resultCode, String message) {
+        Bundle bundle = new Bundle();
+        bundle.putString(DroidConstants.RESULT_DATA_KEY, message);
+        receiver.send(resultCode, bundle);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        if (intent == null) {
+            return;
+        }
+        String errorMessage = "";
+
+        // Get the location passed to this service through an extra.
+        Location location = intent.getParcelableExtra(
+                DroidConstants.LOCATION_DATA_EXTRA);
+
+        // ...
+
+        List<Address> addresses = null;
+
+        try {
+            addresses = geocoder.getFromLocation(
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    // In this sample, get just a single address.
+                    1);
+        } catch (IOException ioException) {
+            // Catch network or other I/O problems.
+            errorMessage = getString(R.string.service_not_available);
+            Log.e(TAG, errorMessage, ioException);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            // Catch invalid latitude or longitude values.
+            errorMessage = getString(R.string.invalid_lat_long_used);
+            Log.e(TAG, errorMessage + ". " +
+                    "Latitude = " + location.getLatitude() +
+                    ", Longitude = " +
+                    location.getLongitude(), illegalArgumentException);
+        }
+
+        // Handle case where no address was found.
+        if (addresses == null || addresses.size()  == 0) {
+            if (errorMessage.isEmpty()) {
+                errorMessage = getString(R.string.no_address_found);
+                Log.e(TAG, errorMessage);
+            }
+            deliverResultToReceiver(DroidConstants.FAILURE_RESULT, errorMessage);
+        } else {
+            Address address = addresses.get(0);
+            ArrayList<String> addressFragments = new ArrayList<String>();
+
+            // Fetch the address lines using getAddressLine,
+            // join them, and send them to the thread.
+            for(int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                addressFragments.add(address.getAddressLine(i));
+            }
+            Log.i(TAG, getString(R.string.address_found));
+            deliverResultToReceiver(DroidConstants.SUCCESS_RESULT,
+                    TextUtils.join(System.getProperty("line.separator"),
+                            addressFragments));
+        }
+    }
+}
